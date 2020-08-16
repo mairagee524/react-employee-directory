51363eca538aa63bb81a80bb926e5646a9e29c36
mairagee524 <garciamaira524@yahoo.com>
2020-08-15 21:21:59 -0700

    renamed app.js file and modified src index.js file

--- directory/src/index.js
+++ directory/src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './app';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
