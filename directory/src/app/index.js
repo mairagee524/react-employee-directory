5cdec243533b78d9a2048349e54d516920e01518
mairagee524 <garciamaira524@yahoo.com>
2020-08-15 23:04:24 -0700

    added routes

--- directory/src/app/index.js
+++ directory/src/app/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import { NavBar } from '../components'
+import { EmployeesList, EmployeesInsert, EmployeesUpdate } from '../pages'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -9,6 +10,15 @@ function App() {
     return (
         <Router>
             <NavBar />
+            <Switch>
+                <Route path="/employees/list" exact component={EmployeesList} />
+                <Route path="/employees/create" exact component={EmployeesInsert} />
+                <Route
+                    path="/employees/update/:id"
+                    exact
+                    component={EmployeesUpdate}
+                />
+            </Switch>
         </Router>
     )
 }
