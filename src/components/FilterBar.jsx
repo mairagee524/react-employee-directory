87308a35f3681e30176d63729f08f00c74904dc4
mairagee524 <garciamaira524@yahoo.com>
2020-08-19 22:20:43 -0700

    modularized EmployeeList file

---
+++ src/components/FilterBar.jsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+const FilterBar = styled.div``
+
+export default FilterBar
---
+++ src/components/OrderButtons.jsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+const FilterButton = styled.button``
+
+export default FilterButton
